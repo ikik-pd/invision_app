ec2d100fdfb87a71216e2ffc6fedf957064011e6
jjin2e <129535521+jjin2e@users.noreply.github.com>
2023-07-02 19:45:54 +0900

    style. 전체 수정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,15 @@
-import {styled} from "styled-components";
+import { styled } from 'styled-components';
 
 const Button = styled.button`
   background-color: #000;
   color: #fff;
-  width: 62px;
-  height: 25px;
+  width: 76px;
+  height: 34px;
   border: none;
   cursor: pointer;
   border-radius: 20px;
+  font-size: 18px;
+  transition: 0.4s;
 
   &:hover {
     background-color: #ff385c;
